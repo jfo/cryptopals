d358a64eee09f6b24cbe77aa6358b7b7f1672fac
Jeff Fowler <jeffowler@gmail.com>
2016-10-29 20:19:03 -0400

    scoring function works

--- src/bin/fixedxor.rs
+++ src/bin/fixedxor.rs
@@ -13,7 +13,6 @@ fn fixed_xor(x: Vec<u8>, y: Vec<u8>) -> Vec<u8>{
 }
 
 fn fixed_xor_one_char(x: &Vec<u8>, y: u8) -> Vec<u8> {
-
     let mut acc = Vec::new();
     for xe in x.iter() {
         acc.push(xe ^ y);
@@ -22,12 +21,30 @@ fn fixed_xor_one_char(x: &Vec<u8>, y: u8) -> Vec<u8> {
     acc
 }
 
-fn main() {
-    let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".from_hex().unwrap();
+fn score_for_spaces(x: &Vec<u8>) -> u8 {
+    let mut acc = 0;
 
-    for x in 0..255 {
-        println!("{}: {}",x, String::from_utf8_lossy(&fixed_xor_one_char(&input, x)));
+    for xe in x {
+        if *xe == 32 {
+            acc += 1
+        }
     }
 
-    println!("\n\n{}", String::from_utf8_lossy(&fixed_xor_one_char(&input, 88)));
+    acc
+}
+
+fn main() {
+    let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".from_hex().unwrap();
+    let mut output = 0;
+    let mut score = 0;
+
+    for x in 0..255 {
+        let current = score_for_spaces(&fixed_xor_one_char(&input, x));
+        if  current > score {
+            score = current;
+            output = x;
+        }
+    }
+
+    println!("\n\n{}", String::from_utf8_lossy(&fixed_xor_one_char(&input, output)));
 }
